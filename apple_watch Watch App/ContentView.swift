924e5b006164b85155c38086a22a18b0ae5f329e
james_ma <t90laser93@gmail.com>
2024-04-13 06:45:23 +0900

    Initial Commit

---
+++ apple_watch Watch App/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  apple_watch Watch App
+//
+//  Created by 마지혁 on 4/13/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ apple_watch Watch App/apple_watchApp.swift
@@ -0,0 +1,17 @@
+//
+//  apple_watchApp.swift
+//  apple_watch Watch App
+//
+//  Created by 마지혁 on 4/13/24.
+//
+
+import SwiftUI
+
+@main
+struct apple_watch_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
